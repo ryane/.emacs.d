c4ac5e3c1c7bc53f4e49862d9aa324fb409ff989
ryane <ryanesc@gmail.com>
2013-10-16 12:31:35 -0400

    turn on recent mode and add ido recent function

---
+++ modules/rae-defuns.el
@@ -0,0 +1,13 @@
+;; Function definitions
+;; lifted from emacs-starter-kit
+
+(recentf-mode 1)
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+;; replaces set-fill-column
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
