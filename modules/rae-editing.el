ecdfc7402cac53d6281f65ef067a745b90506a05
ryane <ryanesc@gmail.com>
2013-10-16 19:55:41 -0400

    setting some editing defaults

---
+++ modules/rae-editing.el
@@ -0,0 +1,23 @@
+;; Editing tweaks
+
+;; tabs and whitespace
+(setq-default indent-tabs-mode nil)
+'(tab-width 2)
+
+;; whitespace defaults
+(setq whitespace-style
+      (quote (face tabs trailing lines space-before-tab
+                   indentation empty space-after-tab tab-mark)))
+
+;; programming defaults
+(defun rae-prog-mode-defaults ()
+  "Default settings for all programming languages"
+  (linum-mode)
+  (whitespace-mode +1))
+(add-hook 'prog-mode-hook 'rae-prog-mode-defaults)
+
+;; text defaults
+(defun rae-text-mode-defaults ()
+  "Default settings for text modes"
+  (whitespace-mode +1))
+(add-hook 'text-mode-hook 'rae-text-mode-defaults)
