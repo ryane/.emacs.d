059b19b7ea6e19e3b1e89d3e990c300a559e4be8
ryane <ryanesc@gmail.com>
2013-10-17 18:46:26 -0400

    turn off whitespace-mode in web-mode since it breaks syntax highlighting

--- modules/rae-web-mode.el
+++ modules/rae-web-mode.el
@@ -9,3 +9,14 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist
 '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
+
+(eval-after-load 'web-mode
+  '(progn
+     (defun rae-web-mode-defaults ()
+       "Default settings for web mode"
+       (linum-mode)
+       ;; whitespace-mode breaks syntax highlighting
+       (whitespace-mode -1))
+     (setq rae-web-mode-hook 'rae-web-mode-defaults)
+     (add-hook 'web-mode-hook ( lambda ()
+                                (run-hooks 'rae-web-mode-hook)))))
