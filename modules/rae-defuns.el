dcbdd5f1543af3aa350dcea7bbfd8b504f811928
ryane <ryanesc@gmail.com>
2013-10-20 12:57:35 -0400

    transpose windows function

--- modules/rae-defuns.el
+++ modules/rae-defuns.el
@@ -10,3 +10,17 @@
 
 ;; replaces set-fill-column
 (global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+
+(defun transpose-windows (arg)
+   "Transpose the buffers shown in two windows."
+   (interactive "p")
+   (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+     (while (/= arg 0)
+       (let ((this-win (window-buffer))
+             (next-win (window-buffer (funcall selector))))
+         (set-window-buffer (selected-window) next-win)
+         (set-window-buffer (funcall selector) this-win)
+         (select-window (funcall selector)))
+       (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+
+(define-key ctl-x-4-map (kbd "t") 'transpose-windows)
