45f2bc9ad6b99766848e1311ed34a57f96ad473c
ryane <ryanesc@gmail.com>
2013-10-19 20:13:25 -0400

    disable ruby-end and add ability to % matchit ruby blocks

--- modules/rae-ruby.el
+++ modules/rae-ruby.el
@@ -6,12 +6,30 @@
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
+(global-rbenv-mode)
+
+;; adapted from
+;; http://ck.kennt-wayne.de/2013/may/emacs%3A-jump-to-matching-paren-beginning-of-block
+(defun goto-matching-ruby-block (arg)
+  (interactive "p")
+  (cond
+   ;; are we at an end keyword?
+   ((equal (current-word) "end")
+    (ruby-beginning-of-block))
+
+   ;; or are we at a keyword itself?
+   ((string-match
+     (current-word)
+     "\\(for\\|while\\|until\\|if\\|class\\|module\\|case\\|unless\\|def\\|begin\\|do\\)")
+    (ruby-end-of-block))))
+
+(defun rae-ruby-evil-overrides ()
+  (define-key evil-normal-state-local-map "%" 'goto-matching-ruby-block))
+
 (defun rae-ruby-mode-defaults ()
   (inf-ruby-minor-mode +1)
   (ruby-tools-mode +1)
   (flymake-ruby-load)
   (flymake-cursor-mode)
-  (ruby-end-mode))
+  (rae-ruby-evil-overrides))
 (add-hook 'ruby-mode-hook 'rae-ruby-mode-defaults)
-
-(global-rbenv-mode)
