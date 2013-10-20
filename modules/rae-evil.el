318473247714f660fb9589b6158411419c6e5445
ryane <ryanesc@gmail.com>
2013-10-19 20:15:08 -0400

    add evil hjkl bindings in magit and occur modes

--- modules/rae-evil.el
+++ modules/rae-evil.el
@@ -37,3 +37,15 @@
 (setq key-chord-two-keys-delay 0.2)
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 (key-chord-mode 1)
+
+;; Make HJKL keys work in special buffers
+(evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
+  "K" 'magit-discard-item
+  "L" 'magit-key-mode-popup-logging)
+(evil-add-hjkl-bindings magit-status-mode-map 'emacs
+  "K" 'magit-discard-item
+  "l" 'magit-key-mode-popup-logging
+  "h" 'magit-toggle-diff-refine-hunk)
+(evil-add-hjkl-bindings magit-log-mode-map 'emacs)
+(evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+(evil-add-hjkl-bindings occur-mode 'emacs)
