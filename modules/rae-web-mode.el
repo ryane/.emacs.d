f4a43d56e1e5c01ef6a8d435b11283d1f365222b
ryane <ryanesc@gmail.com>
2013-10-31 13:39:40 -0400

    .html.erb uses web-mode

--- modules/rae-web-mode.el
+++ modules/rae-web-mode.el
@@ -4,7 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist
