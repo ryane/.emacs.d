6b5801b8f524bdac92c9c5c3f944991ac4b3a70d
ryane <ryanesc@gmail.com>
2013-10-31 13:40:13 -0400

    global key bindings for org-agenda and mu4e

--- modules/rae-ui.el
+++ modules/rae-ui.el
@@ -18,6 +18,9 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "M-l") 'goto-line) ;; formerly ignore downcase-word
 
+(global-set-key (kbd "<f8>") 'org-agenda)
+(global-set-key (kbd "<f9> m") 'rae-run-mu4e)
+
 ;; Font
 (when (memq window-system '(mac ns))
   (set-face-attribute 'default nil :font "Menlo-13"))
