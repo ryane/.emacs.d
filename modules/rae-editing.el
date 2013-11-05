1654ed4710277d25c46c116b61f44308d84ba76a
ryane <ryanesc@gmail.com>
2013-11-05 00:02:22 -0500

    integrate the prelude auto save functionality

--- modules/rae-editing.el
+++ modules/rae-editing.el
@@ -104,3 +104,36 @@
 ;; Integrate hippie-expand with ya-snippet
 (add-to-list 'hippie-expand-try-functions-list
              'yas/hippie-try-expand)
+
+;;;; auto save files
+;; borrowed from emacs prelude https://github.com/bbatsov/prelude
+(defcustom rae-auto-save t
+  "Non-nil values enable auto save."
+  :type 'boolean
+  :group 'rae)
+
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defun rae-auto-save-command ()
+  "Save the current buffer if `rae-auto-save' is not nil."
+  (when (and rae-auto-save
+             buffer-file-name
+             (buffer-modified-p (current-buffer))
+             (file-writable-p buffer-file-name))
+    (save-buffer)))
+
+(defmacro advise-commands (advice-name commands &rest body)
+  "Apply advice named ADVICE-NAME to multiple COMMANDS.
+
+The body of the advice is in BODY."
+  `(progn
+     ,@(mapcar (lambda (command)
+                 `(defadvice ,command (before ,(intern (concat (symbol-name command) "-" advice-name)) activate)
+                    ,@body))
+               commands)))
+
+;; advise all window switching functions
+(advise-commands "auto-save"
+                 (switch-to-buffer other-window windmove-up windmove-down windmove-left windmove-right window-number-select)
+                 (rae-auto-save-command))
+(add-hook 'mouse-leave-buffer-hook 'rae-auto-save-command)
