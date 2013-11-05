965849bb363cd7e4297d7adaba9fb603708ee3c3
ryane <ryanesc@gmail.com>
2013-11-05 15:42:19 -0500

    org-drill customizations

--- modules/rae-custom.el
+++ modules/rae-custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+'(org-drill-optimal-factor-matrix (quote ((1 (2.6 . 4.14) (1.96 . 3.58) (2.5 . 4.0) (2.1799999999999997 . 3.72) (2.36 . 3.86)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
