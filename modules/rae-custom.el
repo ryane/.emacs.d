0f7c75b80ecfa426e5838bf84e0a9e2935ae55c4
ryane <ryanesc@gmail.com>
2013-11-13 12:02:40 -0500

    update org mode line clock color

--- modules/rae-custom.el
+++ modules/rae-custom.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(org-drill-optimal-factor-matrix (quote ((2 (1.8000000000000003 . 2.236) (2.2199999999999998 . 2.277) (2.7 . 2.691) (2.5 . 2.5) (2.46 . 2.443) (2.1799999999999997 . 2.325)) (1 (2.6 . 4.14) (2.36 . 3.86) (2.5 . 4.0) (2.1799999999999997 . 3.72))))))
+ '(org-drill-optimal-factor-matrix (quote ((2 (2.2800000000000002 . 2.418) (2.36 . 2.412) (2.6 . 2.6) (1.8000000000000003 . 2.236) (2.2199999999999998 . 2.277) (2.7 . 2.691) (2.5 . 2.5) (2.46 . 2.443) (2.1799999999999997 . 2.325)) (1 (2.6 . 4.14) (2.36 . 3.86) (2.5 . 4.0) (2.1799999999999997 . 3.72))))))
 '(org-drill-optimal-factor-matrix (quote ((1 (2.6 . 4.14) (1.96 . 3.58) (2.5 . 4.0) (2.1799999999999997 . 3.72) (2.36 . 3.86)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-mode-line-clock ((t (:foreground "#cc6666"))) t))
