29239adc91aafc2f4f2207df7d527ee7dccb6467
ryane <ryanesc@gmail.com>
2013-12-07 14:09:08 -0500

    deft config

---
+++ modules/rae-deft.el
@@ -0,0 +1,6 @@
+(setq deft-extension "txt")
+(setq deft-directory (concat rae-org-dir "/Dropbox/Documents/Organizer/Notes"))
+(setq deft-text-mode 'markdown-mode)
+(setq deft-use-filename-as-title t)
+
+(global-set-key [f7] 'deft)
