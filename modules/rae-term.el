88debaec10f09705821417c57d9e72ea168fc3f3
ryane <ryanesc@gmail.com>
2014-01-12 08:39:02 -0500

    multi-term tweaks

--- modules/rae-term.el
+++ modules/rae-term.el
@@ -1,1 +1,10 @@
 (setq system-uses-terminfo nil)
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq term-buffer-maximum-size 10000)))
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))))
