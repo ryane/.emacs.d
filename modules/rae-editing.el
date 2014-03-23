4438cfbbd1e163ae77be5620b46c2ac13d0de07f
ryane <ryanesc@gmail.com>
2014-03-23 16:39:37 -0400

    move smartparens into own file

--- modules/rae-editing.el
+++ modules/rae-editing.el
@@ -23,11 +23,6 @@
 ;; (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
-;; smart pairing for all
-(require 'smartparens-config)
-(require 'smartparens-ruby)
-(show-smartparens-global-mode +1)
-
 ;; I use single spacing for my sentences
 (setq sentence-end-double-space nil)
 
