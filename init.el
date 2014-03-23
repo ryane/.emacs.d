d613be9c2a2974fde43494d59c371a73da0ba33b
ryane <ryanesc@gmail.com>
2014-03-23 16:41:52 -0400

    use option as meta in emacs-mac build

--- init.el
+++ init.el
@@ -94,3 +94,29 @@
 (server-start)
 
 (setq initial-frame-alist '((top . 0) (left . 0) (width . 176) (height . 53)))
+
+(when (eq system-type 'darwin)
+  ;; from https://gist.github.com/railwaycat/3498096
+  (global-set-key [(hyper a)] 'mark-whole-buffer)
+  (global-set-key [(hyper v)] 'yank)
+  (global-set-key [(hyper c)] 'kill-ring-save)
+  (global-set-key [(hyper s)] 'save-buffer)
+  (global-set-key [(hyper l)] 'goto-line)
+  (global-set-key [(hyper w)]
+                  (lambda () (interactive) (delete-window)))
+  (global-set-key [(hyper z)] 'undo)
+
+  ;; mac switch meta key
+  (defun mac-switch-meta nil
+    "switch meta between Option and Command"
+    (interactive)
+    (if (eq mac-option-modifier nil)
+        (progn
+          (setq mac-option-modifier 'meta)
+          (setq mac-command-modifier 'hyper)
+          )
+      (progn
+        (setq mac-option-modifier nil)
+        (setq mac-command-modifier 'meta)
+        )))
+  (mac-switch-meta))
