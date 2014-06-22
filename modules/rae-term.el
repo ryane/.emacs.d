a5ddc8cfa11ef2445e3b294a1a34711b1dbd961b
ryane <ryanesc@gmail.com>
2014-06-22 07:44:01 -0400

    term mode fixes

--- modules/rae-term.el
+++ modules/rae-term.el
@@ -7,4 +7,5 @@
 (add-hook 'term-mode-hook
           (lambda ()
             (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
-            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))))
+            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+            (define-key term-raw-map (kbd 'C-y') 'term-paste)))
