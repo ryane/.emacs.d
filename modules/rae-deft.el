4860cbd3fa9254fbb77e930fbd9edada45642959
ryane <ryanesc@gmail.com>
2014-10-24 15:12:23 -0400

    update auto save interval for deft

--- modules/rae-deft.el
+++ modules/rae-deft.el
@@ -2,5 +2,6 @@
 (setq deft-directory (concat rae-org-dir "/Dropbox/Documents/Organizer/Notes"))
 (setq deft-text-mode 'markdown-mode)
 (setq deft-use-filename-as-title t)
+(setq deft-auto-save-interval 60.0)
 
 (global-set-key [f7] 'deft)
